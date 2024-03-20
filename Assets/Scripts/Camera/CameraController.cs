bfd73a11aba09b68165b77f7173099c0cd25a040
Arrvialm <2909405306@qq.com>
2024-03-20 20:38:06 +0800

    scene: add Level1

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(Cinemachine.CinemachineVirtualCamera))]
+public class CameraController : MonoBehaviour
+{
+
+    void Update()
+    {
+
+    }
+}
