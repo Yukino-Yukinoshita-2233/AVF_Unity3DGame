a28aafaa12b903274e3e032ef21c977303058c6b
AdamLff <921078100@qq.com>
2024-04-10 19:21:46 +0800

    修改行星运行参数

--- Assets/Scripts/Star/RotateManager.cs
+++ Assets/Scripts/Star/RotateManager.cs
@@ -7,13 +7,13 @@ public class RotateManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> Star = new List<GameObject>();
     float[,] StarData ={
-            { 1,1,1,1},//Sun
-            { 2,2,2,2},//Watch
-            { 3,3,3,3},//Venus
-            { 4,4,4,4},//Earth
-            { 5,5,5,5},//Moon
-            { 6,6,6,6},//Mars
-            { 7,7,7,7} //Wood
+            { 1,10,0,0},//Sun
+            { 2,58,47.87f,6},//Water
+            { 3,243,35.02f,8},//Venus
+            { 4,1,29.78f,10},//Earth
+            { 5,30,1,2},//Moon
+            { 6,1,24.13f,12},//Mars
+            { 7,0.375f,13.07f,15} //Wood
         };
 
     void Start()
@@ -23,8 +23,8 @@ public class RotateManager : MonoBehaviour
             Rotate rotate = Star[i].GetComponent<Rotate>();
             rotate.centerPoint = Star[0].transform;
             if (i == 4){rotate.centerPoint = Star[3].transform;}
-            rotate.rotationSpeed = StarData[i, 1];
-            rotate.orbitSpeed = StarData[i, 2];
+            rotate.rotationSpeed = 24/StarData[i, 1];
+            rotate.orbitSpeed = StarData[i, 2]/10;
             rotate.distanceFromCenter = StarData[i, 3];
         }
     }
