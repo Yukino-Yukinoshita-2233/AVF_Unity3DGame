7974b0ea699876b2824c2fdfc7aabb01d4300bf5
AdamLff <921078100@qq.com>
2024-06-03 21:02:39 +0800

    change scene

---
+++ Assets/Scripts/LV1/OpenGuide.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class OpenGuide : MonoBehaviour
+{
+    public Canvas canvas;  // 要打开的Canvas对象
+
+    // 方法用于打开Canvas
+    void Start()
+    {
+        if (canvas != null)
+        {
+            canvas.gameObject.SetActive(false); // 初始状态下隐藏画布
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (canvas != null)
+            {
+                canvas.gameObject.SetActive(!canvas.gameObject.activeSelf); // 切换画布的显示状态
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LV1/nextimage.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nextimage : MonoBehaviour
+{
+    public Image[] images; // 要切换的图片数组
+    private int currentIndex = 0; // 当前显示的图片索引
+
+    void Start()
+    {
+        UpdateImages();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SwitchToNextImage();
+        }
+    }
+
+    public void SwitchToNextImage()
+    {
+        currentIndex = (currentIndex + 1) % images.Length; // 切换到下一个图片索引
+        UpdateImages();
+    }
+
+    private void UpdateImages()
+    {
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].gameObject.SetActive(i == currentIndex); // 仅显示当前索引对应的图片
+        }
+    }
+}
